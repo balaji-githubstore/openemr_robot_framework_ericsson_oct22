08937eb12dc3a7d7e1c6fc583504ef911d11340f
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-10-12 15:25:21 +0530

    get_driver_path

--- Resource/Base/DriverSetup.py
+++ Resource/Base/DriverSetup.py
@@ -2,7 +2,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
-def driver_path(browser_name):
+def get_driver_path(browser_name):
     if browser_name.lower() == 'firefox':
         path=GeckoDriverManager().install()
     elif browser_name.lower()=='edge':
